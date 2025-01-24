fb9cc0d9e541feb07e0192d2538c0bdd8f0115fb
lucaschennn <lucasch@umich.edu>
2025-01-23 23:28:46 -0500

    add better nav indicator when clicking

--- lucassite/src/components/Nav.jsx
+++ lucassite/src/components/Nav.jsx
@@ -66,7 +66,7 @@ function Intro({selected, setPage}) {
         }}>
             <div style={{
                 position: 'relative',
-                top: `${navGradient}%`,
+                top: `${navGradient + 8}%`,
                 boxShadow: '-60px 0px 65px 15px #ccc',
                 display: `${vertical ? '' : 'none'}`,
             }}>
